25435940ee3fac610cbcda4512b28caa0e7fe368
sgurram <srikanth.gurram@soctronics.com>
2017-01-06 14:28:49 +0530

    ui refinements

--- assets/js/angular/app.js
+++ assets/js/angular/app.js
@@ -1,45 +1,25 @@
 var mainModule = angular.module('myBlog', []);
 mainModule.controller('homeController', function() {
-	    this.blogTitle= "";
-	    this.enteredText="";
-	    this.authorName="";
-	    this.composeBlog=false;
+	// initalize your variables
+    this.blogTitle= "";
+    this.enteredText="";
+    this.authorName="";
+    this.composeBlog=false;
+	this.blogContent=[];
 
-	  /*  this.blogContent = [{
-		title: 'My First blog posted here',
-		author: 'Srikanth',
-		createdDate: 'Jan 01, 2017',
-		content:'Hello guys, Good morning !!',
-		published: true
-	},{
-		title: 'My second blog posted here',
-		author: 'Manasree',
-		createdDate: 'Jan 05, 2017',
-		content:'Hello guys,what\'s going on !!',
-		published: true
-
-	},
-	{
-		title: 'My third blog posted here',
-		author: 'Chaithu',
-		createdDate: 'Jan 05, 2017',
-		content:'Hello guys,what\'s up !!',
-		published: true
-
-	}]
-*/
-    this.blogContent=[];
+	// form method append the data to array
 	this.updateBlog = function() {
 			var temp={};
 			temp.title= this.blogTitle;
 			temp.author= this.authorName;
-			temp.createdDate='06-01-2017';
+			temp.createdDate= new Date();
 			temp.content=this.enteredText;
 			temp.published=true;
 			this.blogContent.push(temp);
 
 	}
 
+	// display the form when click on 'Add' button
 	this.createNewForm = function() {
 		this.composeBlog= true;
 	}
